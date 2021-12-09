e7c024d8046fceefbf39526b0ce81b8646332b4b
Ivan Slabbert <ivan@slabbert.org>
2021-12-09 10:34:57 +0200

    Update _outputAudioDevice base class w. PCM params

--- class/_outputAudioDevice.js
+++ class/_outputAudioDevice.js
@@ -19,6 +19,9 @@ class _outputAudioDevice extends _device {
     constructor(DeviceList) {
         super(DeviceList);
         this.stdin = undefined;             // stdin mapped to process stdin
+        this.channels = 1;                  // Audio channels
+        this.sampleRate = 48000;            // Audio sample rate
+        this.bitDepth = 16;                 // Audio bit depth
     }
 }
 
