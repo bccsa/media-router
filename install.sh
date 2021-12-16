8e443ec086ba7d6df74ae05d2bbb1dc2233a7c38
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2021-12-16 08:30:39 +0200

    Update installer script

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-apt-get -y update
-apt-get -y install nodejs npm
-apt-get -y install alsa-utils
-apt-get -y install ffmpeg
-apt-get -y install git
-apt-get -y install tclsh
-apt-get -y install tcl
-apt-get -y install pkg-config
-apt-get -y install cmake
-apt-get -y install libssl-dev
-apt-get -y install build-essential
+sudo apt-get -y update
+sudo apt-get -y install nodejs npm
+sudo apt-get -y install alsa-utils
+sudo apt-get -y install ffmpeg
+sudo apt-get -y install git
+sudo apt-get -y install tclsh
+sudo apt-get -y install tcl
+sudo apt-get -y install pkg-config
+sudo apt-get -y install cmake
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install build-essential
 
 wget https://github.com/Haivision/srt/archive/refs/tags/v1.4.4.tar.gz
 tar -xzf v1.4.4.tar.gz
@@ -17,11 +17,29 @@ tar -xzf v1.4.4.tar.gz
 cd srt-1.4.4/
 ./configure
 make
-make install
+sudo make install
 
 cd ..
 rm -r -f srt-1.4.4/
 rm -f v1.4.4.tar.gz
 
-#git submodule update --init
-#npm install
+git submodule update --init
+npm install
+
+sudo echo "[Unit]
+Description=Media Router
+After=network.target
+
+[Service]
+WorkingDirectory=${$PWD}
+ExecStart=${$PWD}/router.sh
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /lib/systemd/system/media-router.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable media-router
+sudo systemctl restart media-router
+
