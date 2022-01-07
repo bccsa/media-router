c34ed0309f4922824a398f10b426d8fc6f7b214f
THISmann <fuhetienne0@gmail.com>
2022-01-07 10:10:01 +0100

    uibutton

---
+++ html/controls/uiButton.js
@@ -0,0 +1,29 @@
+// =====================================
+// button with value
+// user control
+//
+// Copyright BCC South Africa
+// =====================================
+
+// -------------------------------------
+// Class declaration
+// -------------------------------------
+
+class uiButton extends _uiControl {
+  constructor() {
+    super();
+    this.value = "click here";
+    this.styles.push("controls/css/bootstrap.min.css");
+  }
+
+  // -------------------------------------
+  // Getters & setters
+  // -------------------------------------
+
+  get html() {
+    return `
+        <!-- ${this.name} --> 
+        <button class="btn btn-default id="${this._uuid}_button" value="${this.value}" /> 
+        `;
+  }
+}
--- html/testControls.js
+++ html/testControls.js
@@ -3,38 +3,37 @@ const controlsDiv = document.getElementById(`controls`);
 const domHead = document.getElementsByTagName(`head`)[0];
 
 let _abortController = new AbortController();
-document.addEventListener('DOMContentLoaded', _init, { signal: _abortController.signal } );
-// window.addEventListener('load', _init(), { signal: _abortController.signal } );
+document.addEventListener("DOMContentLoaded", _init, {
+  signal: _abortController.signal,
+});
 
-// Control initialization
 function _init() {
-    _abortController.abort();
-    
+  _abortController.abort();
 
-    const textBox1 = new uiTextBox();
-    textBox1.displayName = "Name";
-    controlsDiv.innerHTML += textBox1.html;
+  const textBox1 = new uiTextBox();
+  textBox1.displayName = "Name";
+  controlsDiv.innerHTML += textBox1.html;
+  textBox1.value = " etienne fuh";
 
-    // const textBox3 = new uiTextBox();
-    // textBox3.displayName = "Class";
-    // controlsDiv.innerHTML += textBox3.html;
+  textBox1.DomLinkup();
 
-    textBox1.DomLinkup();
-    // textBox1.styles.forEach(ref => {
-    //     domHead.innerHTML += `<link rel="stylesheet" href="${ref}"></link>`;
-    // })
+  // Child controls
+  const textBox2 = new uiTextBox();
+  textBox2.value = "test value fuh";
+  textBox1.AddControl(textBox2);
 
-    // Child controls
-    const textBox2 = new uiTextBox();
-    textBox2.value = "test value fuh";
-    textBox1.AddControl(textBox2);
+  textBox2.DomLinkup();
 
- 
+  // New ui control
+
+  const textBox3 = new uiTextBox();
+  textBox3.displayName = "Class";
+  controlsDiv.innerHTML += textBox3.html;
+  textBox3.value = "AB Testing";
+
+  textBox3.DomLinkup();
 }
 
 // Top level parent control
 
-// 
-
-
-
+//
