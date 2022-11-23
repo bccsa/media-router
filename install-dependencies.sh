347d59aca89c969c92d28783ac799ffd26964111
Ivan Slabbert <ivan@slabbert.org>
2022-11-23 09:11:36 +0200

    update install scripts

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -26,25 +26,3 @@ rm -f v1.5.1.tar.gz
 
 git submodule update --init
 npm install
-
-echo "[Unit]
-Description=Media Router
-After=network.target
-
-[Service]
-WorkingDirectory=$PWD
-ExecStart=$PWD/router.sh
-User=$USER
-Group=$USER
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-" | sudo tee /lib/systemd/system/media-router.service
-
-chmod +x router.sh
-sudo systemctl daemon-reload
-sudo systemctl enable media-router
-sudo systemctl restart media-router
-
-echo "@sh $PWD/clientUI.sh" | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
