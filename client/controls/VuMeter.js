c386ded2947e98f1735032b5741d3d1f150d966c
Albert Willemse <albertjordaanw@gmail.com>
2022-11-29 10:44:24 +0200

    VuMeter - Horizontal

--- client/controls/VuMeter.js
+++ client/controls/VuMeter.js
@@ -52,6 +52,7 @@ class VuMeter extends ui {
         switch (propertyName) {
             case "level":
                 //this._setLevelVertical();
+                //this._setLevelHorizontal();
                 break;
             default:
                 break;
@@ -96,6 +97,56 @@ class VuMeter extends ui {
                 if (this._top3Prev > bot3) bot = bot3;
                 this._ctx.fillStyle = "red";
                 this._ctx.fillRect(0, top3, this._width, bot - top3 + 1);
+            }
+        }
+
+        this._top1Prev = top1;
+        this._bot1Prev = bot1;
+        this._top2Prev = top2;
+        this._bot2Prev = bot2;
+        this._top3Prev = top3;
+        this._bot3Prev = bot3;
+        this._totalPrev = total;
+    }
+
+    _setLevelHorizontal() {
+        // Logarithmic level in 50 steps
+        let p = Math.round(20 * Math.log10(this.level) * 50) / 50;
+
+        let width1 = Math.min(Math.max((p + 60), 0), 60 - 20) * this._width / 60;  // Start showing from -60dB. Max width at -20dB (40dB width)
+        let width2 = Math.min(Math.max((p + 20), 0), 20 - 9) * this._width / 60;   // Start showing from -20dB. Max width at -9dB (11dB width)
+        let width3 = Math.min(Math.max((p + 9), 0), 9 - 0) * this._width / 60;     // Start showing from -9dB. Max width at -0dB (9dB width)
+
+        let bot1 = this._width;
+        let top1 = this._width - width1;
+        let bot2 = top1;
+        let top2 = bot2 - width2;
+        let bot3 = top2;
+        let top3 = bot3 - width3;
+
+        let total = width1 + width2 + width3;
+
+        // Clear
+        if (total < this._totalPrev) {
+            this._ctx.clearRect(this._top3Prev, 0, top3 - this._top3Prev, this._width);
+        }
+        // Draw
+        else if (total > this._totalPrev) {
+            if (top1 < this._top1Prev) {
+                this._ctx.fillStyle = "green";
+                this._ctx.fillRect(top1, 0, this._top1Prev - top1 + 1, this._width);
+            }
+            if (top2 < this._top2Prev && top2 < bot2) {
+                let bot = this._top2Prev;
+                if (this._top2Prev > bot2) bot = bot2;
+                this._ctx.fillStyle = "orange";
+                this._ctx.fillRect(top2, 0, bot - top2 + 1, this._width);
+            }
+            if (top3 < this._top3Prev && top3 < bot3) {
+                let bot = this._top3Prev;
+                if (this._top3Prev > bot3) bot = bot3;
+                this._ctx.fillStyle = "red";
+                this._ctx.fillRect(top3, 0, bot - top3 + 1, this._width);
             }
         }
 
