f443cde1f0d47c85cd277db3388985af92b42832
Ivan Slabbert <ivan@slabbert.org>
2023-01-19 10:17:36 +0200

    Add support for extended classes

--- client/manager.js
+++ client/manager.js
@@ -1,5 +1,6 @@
 var controls = new uiTopLevelContainer('../controls', 'controls');
 
+// Add appFrame control
 controls.SetData({
     appFrame: {
         controlType: "appFrame"
@@ -30,79 +31,4 @@ controls.on('appFrame', appFrame => {
     });
 });
 
-let pList = [];
-(controls.LoadScript("_audioDevice" + ".js")).then(() => {
-    controls.LoadScript("_audioInputDevice" + ".js").then(() => {
 
-
-
-
-        // controls.SetData({
-
-        //     DeviceList1: {
-        //         controlType: "DeviceList",
-
-        //         SrtOpusOutput: {
-        //             controlType: "SrtOpusOutput"
-        //         },
-
-        //         SrtOpusInput: {
-        //             controlType: "SrtOpusInput"
-        //         },
-
-        //         AudioInput: {
-        //             controlType: "AudioInput"
-        //         },
-
-        //         AudioOutput: {
-        //             controlType: "AudioOutput"
-        //         }
-        //     },
-
-        //     DeviceList2: {
-        //         controlType: "DeviceList",
-
-        //         SrtOpusOutput: {
-        //             controlType: "SrtOpusOutput"
-        //         },
-
-        //         SrtOpusInput: {
-        //             controlType: "SrtOpusInput"
-        //         },
-
-        //         AudioInput: {
-        //             controlType: "AudioInput"
-        //         },
-
-        //         AudioOutput: {
-        //             controlType: "AudioOutput"
-        //         }
-        //     }
-
-        // });
-    });
-});
-
-
-// function toggleContainer(uuidContainer,uuidButton) {
-//     var container = document.getElementById(uuidContainer);
-//     var toggleButton = document.getElementById(uuidButton);
-//     if (container.style.display === "none") {
-//         container.style.display = "block";
-//         toggleButton.textContent = "▲";
-//     } else {
-//         container.style.display = "none";
-//         toggleButton.textContent = "▼";
-//     }
-// }
-
-// function toggleMute(uuid) {
-//     var myAudio = document.getElementById(uuid);
-//     myAudio.muted = !myAudio.muted;
-//  }
-
-//  var un_mute = document.getElementById('un-mute');
-
-// un_mute.onclick = function() {
-//    alert('toggle player here');
-// };
