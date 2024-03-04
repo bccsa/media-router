a78bf4e59d489cf65a1de02da3802b5b90a7e47f
Oswald SLabbert <Oswald@slabbert.org>
2024-03-04 11:13:15 +0200

    hotfix/ videoplayer max-size-time

--- server/gst_modules/SrtVideoPlayer/src/SrtVideoPlayer.h
+++ server/gst_modules/SrtVideoPlayer/src/SrtVideoPlayer.h
@@ -247,7 +247,7 @@ void _SrtVideoPlayer::th_Start() {
     g_object_set (gl.kmssink, "connector-id", 32, NULL); 
     // queue's
     g_object_set (gl.a_convert_queue, "leaky", 2, NULL); 
-    g_object_set (gl.a_convert_queue, "max-size-time ", 1000000, NULL); 
+    g_object_set (gl.a_convert_queue, "max-size-time", 100000000, NULL); 
 
     /* Link all elements that can be automatically linked because they have "Always" pads */
     gst_bin_add_many (GST_BIN (this->pipeline), gl.source, gl.src_queue, gl.tsdemux,                                        // src
