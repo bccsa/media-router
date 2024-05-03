79c0371688dfeb11b943817a387e917fb5e98b5c
Oswald <Oswald@slabbert.org>
2024-05-03 09:08:52 +0200

    hotfix/ added opusparse and tsparse, needed for demuxer and decoder to process packets

--- server/controls/SrtOpusInput.js
+++ server/controls/SrtOpusInput.js
@@ -17,7 +17,7 @@ class SrtOpusInput extends Classes(_paNullSinkBase, SrtBase) {
             if (ready) {
                 let _pipeline = `srtserversrc name=${this._srtElementName} uri="${this.uri()}" wait-for-connection=false ! ` +
                 `tsdemux ignore-pcr=true latency=1 ! ` +
-                `opusdec use-inband-fec=true plc=true ! ` + 
+                `opusparse ! opusdec use-inband-fec=true plc=true ! ` + 
                 `queue leaky=2 max-size-time=100000000 flush-on-eos=true ! ` + 
                 `audioconvert ! audioresample ! ` +
                 `pulsesink device="${this.sink}" sync=false buffer-time=${this._parent.paLatency * 1000} max-lateness=${this._parent.paLatency * 1000000}`
