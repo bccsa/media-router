42cf4dba495e5985f5cf589a1609f287db855708
Oswald <Oswald@slabbert.org>
2024-05-20 16:59:55 +0200

    sidechain channel map

--- server/controls/SoundDucking.js
+++ server/controls/SoundDucking.js
@@ -7,8 +7,6 @@ const vu = new vuMeter();
 class SoundDucking extends _paNullSinkBase {
     constructor() {
         super();
-        this.side_chain = "";       // chain to listen on
-        this.channelMap = '1,2';
         this.threshold = 60;        // level where to activate 
         this.ducking_level = 30;    // level to drop audio to
         this.attack = 20;           // attack in ms (time before ducking)
@@ -26,6 +24,10 @@ class SoundDucking extends _paNullSinkBase {
         this._attackInterval = undefined;
         this._releaseInterval = undefined;
         this._currentVol = this.volume;
+
+        // side chain
+        this.side_chain = "";       // chain to listen on
+        this.channelMap = '';
     }
 
     Init() {
@@ -35,6 +37,7 @@ class SoundDucking extends _paNullSinkBase {
         this.on('ready', ready => {
             vu.ready = ready;
             if (ready) {
+                this._map();
                 this._parent.PaCmdQueue(() => {
                     this._startAudioDucker();
                 });
@@ -50,15 +53,66 @@ class SoundDucking extends _paNullSinkBase {
             }
         });
 
+        this.on('side_chain', () => {
+            this._map();
+        });
+
+        this.on('channelMap', () => {
+            this._map();
+        });
+
         this.on('threshold', val => {
             this._threshold = (val / 100 * 60) -60; // offset with -60 to make a negative
         }, { immediate: true })
     }
 
+    /**
+     * Calculates and sets this.channels and this._channelMap from this.channelMap
+     */
+    _map() {
+        if (this._parent._sources[this.side_chain]) {
+            let _srcChannels = this._parent._sources[this.side_chain].channels;
+            let _srcChannelMap = this._parent._sources[this.side_chain].channelmap;
+
+            let masterMap = [];
+            let channelCount = 0;
+            let channelMap = [];
+
+            this.channelMap.split(',').forEach(channel => {
+                let ch = parseInt(channel);
+                if (ch && ch > 0 && ch <= _srcChannels) {
+                    masterMap.push(_srcChannelMap[ch - 1]);
+                    channelMap.push(ch);
+                    channelCount++;
+                }
+            });
+
+            // Reduce channel count if larger than side_chain channel count
+            if (channelCount > _srcChannels) {
+                channelCount = _srcChannels;
+                channelMap.splice(channelCount);
+            }
+
+            if (channelCount > 0) {
+                this.channelMap = channelMap.join(',');
+            } else {
+                // Create default channel map
+                channelMap = [];
+                for (let i = 1; i <= _srcChannels; i++) {
+                    channelMap.push(i);
+                }
+                this.channelMap = channelMap.join(',');
+
+                // Regenerate map
+                this._map();
+            }
+        }
+    }
+
     _startAudioDucker() {
         if (this.side_chain) {
             this._parent._log('INFO', `${this._paModuleName} (${this.displayName}): Starting Audio Ducker`);
-            const _pl = `pulsesrc device=${this.side_chain} ! audio/x-raw,rate=${this.sampleRate},format=S${this.bitDepth}LE,channels=1 ! audioconvert mix-matrix="<<(float)1.0>>" ! queue ! level peak-falloff=120 peak-ttl=50000000 interval=${this.vuInterval * 1000000} ! fakesink silent=true`;
+            const _pl = `pulsesrc device=${this.side_chain} ! audio/x-raw,rate=${this.sampleRate},format=S${this.bitDepth}LE,channels=1 ! audioconvert mix-matrix="<<${this.channelMap}>>" ! queue ! level peak-falloff=120 peak-ttl=50000000 interval=${this.vuInterval * 1000000} ! fakesink silent=true`;
             const _path = `${path.dirname(process.argv[1])}/child_processes/vu_child.js`;
             vu.start_vu(_path, [_pl, "audioLevel"]);
             // start vu
