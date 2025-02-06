daba6431cd4758133cc656cfe9ce9c5b26b9bc95
oswald <oswald@slabbert.org>
2025-02-06 15:17:43 +0200

    removed ignore-pcr=true from tsdemux, this improves the stability with a lot, due to the demuxer was struggeling to sync on the frames it received

--- server/controls/SrtVideoPlayer.js
+++ server/controls/SrtVideoPlayer.js
@@ -18,7 +18,7 @@ class SrtVideoPlayer extends Classes(_paNullSinkBase, SrtBase) {
                 this._parent._log('INFO', `${this._controlName} (${this.displayName}): Starting srt video decoder (gstreamer)`);
 
                 let _pipeline = `srtserversrc name=${this._srtElementName} uri="${this.uri()}" wait-for-connection=false ! ` +
-                `tsparse ignore-pcr=true ! tsdemux ignore-pcr=true latency=1 name=t t. ! ` +
+                `tsparse ignore-pcr=true ! tsdemux latency=1 name=t t. ! ` +
                 `h264parse ! v4l2h264dec ! ` + 
                 `queue flush-on-eos=true leaky=2 max-size-time=50000000 ! ` +
                 `kmssink sync=false async=false t. ! ` + 
