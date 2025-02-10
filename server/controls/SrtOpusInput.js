34dc3ffc2a61be1dce525e694d9cd271b2108b06
oswald <oswald@slabbert.org>
2025-02-10 13:20:53 +0200

    fixed startup of modules to check for ready and run

--- server/controls/SrtOpusInput.js
+++ server/controls/SrtOpusInput.js
@@ -13,28 +13,29 @@ class SrtOpusInput extends Classes(_paNullSinkBase, SrtBase) {
         super.Init();
 
         // Start external processes when the underlying pipe-source is ready (from extended class)
-        this.on('ready', ready => {
-            if (ready) {
-                let _pipeline = `srtserversrc name=${this._srtElementName} uri="${this.uri()}" wait-for-connection=false ! ` +
-                `tsparse ignore-pcr=true ! tsdemux ignore-pcr=true latency=1 ! ` +
-                `opusdec use-inband-fec=true plc=true ! ` + 
-                `audioconvert ! ` +
-                `audio/x-raw,rate=${this.sampleRate},format=S${this.bitDepth}LE,channels=${this.channels},channel-mask=(bitmask)0x${(Math.pow(2, this.channels) -1).toString(16)} ! ` +
-                `queue leaky=2 max-size-time=50000000 flush-on-eos=true ! ` + 
-                `pulsesink device="${this.sink}" sync=false slave-method=0 processing-deadline=40000000 buffer-time=50000 max-lateness=50000000`
-
-                this._parent.PaCmdQueue(() => { 
-                    this._start_srt(`node ${path.dirname(process.argv[1])}/child_processes/SrtGstGeneric_child.js '${_pipeline}'`, this._srtElementName);
-                });
-            }
-        });
+        this.on('ready', ready => { this.startPipeline() });
 
         // Stop external processes when the control is stopped (through setting this.run to false)
         this.on('run', run => {
-            if (!run) {
-                this._stop_srt();
-            }
+            if (!run) this._stop_srt();
+            this.startPipeline()
         });
+    }
+
+    startPipeline() {
+        if (this.ready && this.run) {
+            let _pipeline = `srtserversrc name=${this._srtElementName} uri="${this.uri()}" wait-for-connection=false ! ` +
+            `tsparse ignore-pcr=true ! tsdemux ignore-pcr=true latency=1 ! ` +
+            `opusdec use-inband-fec=true plc=true ! ` + 
+            `audioconvert ! ` +
+            `audio/x-raw,rate=${this.sampleRate},format=S${this.bitDepth}LE,channels=${this.channels},channel-mask=(bitmask)0x${(Math.pow(2, this.channels) -1).toString(16)} ! ` +
+            `queue leaky=2 max-size-time=50000000 flush-on-eos=true ! ` + 
+            `pulsesink device="${this.sink}" sync=false slave-method=0 processing-deadline=40000000 buffer-time=50000 max-lateness=50000000`
+
+            this._parent.PaCmdQueue(() => { 
+                this._start_srt(`node ${path.dirname(process.argv[1])}/child_processes/SrtGstGeneric_child.js '${_pipeline}'`, this._srtElementName);
+            });
+       }
     }
 
 }
