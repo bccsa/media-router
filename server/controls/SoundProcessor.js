323d090cf435ae9bfb5111700ef94fd9b3533515
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2025-02-13 11:00:35 +0200

    343 permission denied on sound processor (#345)
    * fixed ducker and sound processor to detect when _paAudioSinkBase is not available
    
    * Fixed sound processor to startup correctly

--- server/controls/SoundProcessor.js
+++ server/controls/SoundProcessor.js
@@ -1,29 +1,29 @@
-const _paNullSinkSourceBase = require('./_paNullSinkSourceBase');
-const path = require('path');
-const GstBase = require('./GstBase');
-const { Classes } = require('../modular-dm');
+const _paNullSinkSourceBase = require("./_paNullSinkSourceBase");
+const path = require("path");
+const GstBase = require("./GstBase");
+const { Classes } = require("../modular-dm");
 
 class SoundProcessor extends Classes(_paNullSinkSourceBase, GstBase) {
     constructor() {
         super();
         // EQ settings
-        this.eq = false;            // Enable opus Forward Error Correction
-        this.band0 = 0;             // Band 0 gain
-        this.band1 = 0;             // Band 1 gain
-        this.band2 = 0;             // Band 2 gain
-        this.band3 = 0;             // Band 3 gain
-        this.band4 = 0;             // Band 4 gain
-        this.band5 = 0;             // Band 5 gain
-        this.band6 = 0;             // Band 6 gain
-        this.band7 = 0;             // Band 7 gain
-        this.band8 = 0;             // Band 8 gain
-        this.band9 = 0;             // Band 9 gain
-        
-        //Delay Settings
-        this.delay = false          // Enable Delay
-        this.delayVal = 0;          // Delay Value
+        this.eq = false; // Enable opus Forward Error Correction
+        this.band0 = 0; // Band 0 gain
+        this.band1 = 0; // Band 1 gain
+        this.band2 = 0; // Band 2 gain
+        this.band3 = 0; // Band 3 gain
+        this.band4 = 0; // Band 4 gain
+        this.band5 = 0; // Band 5 gain
+        this.band6 = 0; // Band 6 gain
+        this.band7 = 0; // Band 7 gain
+        this.band8 = 0; // Band 8 gain
+        this.band9 = 0; // Band 9 gain
 
-        // Gate / Expander 
+        //Delay Settings
+        this.delay = false; // Enable Delay
+        this.delayVal = 0; // Delay Value
+
+        // Gate / Expander
         this.gate = false;
         this.gate_knee = 2.8;
         this.gate_ratio = 1;
@@ -46,35 +46,13 @@ class SoundProcessor extends Classes(_paNullSinkSourceBase, GstBase) {
     Init() {
         super.Init();
         // Start external processes when the underlying null-sink is ready (from extended class)
-        this.on('ready', ready => {
-            if (ready) {
-                // Source
-                let _pipeline = `pulsesrc device=${this._source} latency-time=${this._parent.paLatency * 1000} buffer-time=${this._parent.paLatency * 1000} ! ` + // do-timestamp=false used for live audio (see: https://github.com/ros-drivers/audio_common/issues/120)
-                `audio/x-raw,rate=${this.sampleRate},format=S${this.bitDepth}LE,channels=${this.channels},channel-mask=(bitmask)0x${(Math.pow(2, this.channels) -1).toString(16)} ! audioconvert ! audiorate ! `;
-                // EQ
-                _pipeline += `equalizer-10bands name="eq" ${this._bands()} ! `; 
-                // Gate
-                _pipeline += `calf-sourceforge-net-plugins-Gate name="gate" bypass=${!this.gate} knee=${this.gate_knee} ratio=${this.gate_ratio} threshold=${this.gate_threshold} attack=${this.gate_attack} release=${this.gate_release} makeup=${this.gate_makeup} ! `;
-                // Compressor
-                _pipeline += `calf-sourceforge-net-plugins-Compressor name="compressor" bypass=${!this.compressor} knee=${this.comp_knee} ratio=${this.comp_ratio} threshold=${this.comp_threshold} makeup=${this.comp_makeup} mix=${this.comp_mix} attack=${this.comp_attack} release=${this.comp_release} ! `;
-                // Delay
-                if (this.delay)
-                _pipeline += `queue name="delay" leaky=2 min-threshold-time=${this.delayVal * 1000000} max-size-buffers=0 max-size-bytes=0 max-size-time=${(this.delayVal + 100) * 1000000} ! `
-                
-                // Sink 
-                _pipeline += `pulsesink device=${this._sink} sync=false buffer-time=${this._parent.paLatency * 1000} max-lateness=${this._parent.paLatency * 1000000}`;
-
-                // ------------ start sound processor ------------ //
-                this._parent.PaCmdQueue(() => { 
-                    this._start_gst(`${path.dirname(process.argv[1])}/child_processes/SrtGstGeneric_child.js`, [
-                        _pipeline
-                    ]);
-                });
-            }
+        this.on("ready", () => {
+            this.startPipeline();
         });
 
         // Stop external processes when the control is stopped (through setting this.run to false)
-        this.on('run', run => {
+        this.on("run", (run) => {
+            this.startPipeline();
             if (!run) {
                 this._stop_gst();
             }
@@ -82,86 +60,85 @@ class SoundProcessor extends Classes(_paNullSinkSourceBase, GstBase) {
 
         // ------------ EQ ------------ //
 
-        // Add eventListeners for band's 
+        // Add eventListeners for band's
         for (let i = 0; i < 10; i++) {
-            this.on(`band${i}`, val => {
-                if (this.eq)
-                    this.set_gst("eq", "gdouble", `band${i}`, val);
-            })
+            this.on(`band${i}`, (val) => {
+                if (this.eq) this.set_gst("eq", "gdouble", `band${i}`, val);
+            });
         }
 
         // listen on EQ change
-        this.on("eq", val => {
+        this.on("eq", (val) => {
             this._setBands();
-        })
+        });
 
         // ------------ EQ ------------ //
 
         // ------------ Audio Gate ------------ //
 
-        this.on("gate", val => {
+        this.on("gate", (val) => {
             this.set_gst("gate", "bool", `bypass`, !val);
-        })
+        });
 
-        this.on("gate_knee", val => {
+        this.on("gate_knee", (val) => {
             this.set_gst("gate", "gdouble", `knee`, val);
-        })
+        });
 
-        this.on("gate_ratio", val => {
+        this.on("gate_ratio", (val) => {
             this.set_gst("gate", "gdouble", `ratio`, val);
-        })
+        });
 
-        this.on("gate_threshold", val => {
+        this.on("gate_threshold", (val) => {
             this.set_gst("gate", "gdouble", `threshold`, val);
-        })
+        });
 
-        this.on("gate_attack", val => {
+        this.on("gate_attack", (val) => {
             this.set_gst("gate", "gdouble", `attack`, val);
-        })
+        });
 
-        this.on("gate_release", val => {
+        this.on("gate_release", (val) => {
             this.set_gst("gate", "gdouble", `release`, val);
-        })
+        });
 
-        this.on("gate_makeup", val => {
+        this.on("gate_makeup", (val) => {
             this.set_gst("gate", "gdouble", `makeup`, val);
-        })
+        });
 
         // ------------ Audio Gate ------------ //
 
         // ------------ Audio Compressor ------------ //
 
-        this.on("compressor", val => {
+        this.on("compressor", (val) => {
             this.set_gst("compressor", "bool", `bypass`, !val);
-        })
+        });
 
-        this.on("comp_knee", val => {
+        this.on("comp_knee", (val) => {
             this.set_gst("compressor", "gdouble", `knee`, val);
-        })
+        });
 
-        this.on("comp_ratio", val => {
+        this.on("comp_ratio", (val) => {
             this.set_gst("compressor", "gdouble", `ratio`, val);
-        })
+        });
 
-        this.on("comp_threshold", val => {
+        this.on("comp_threshold", (val) => {
             this.set_gst("compressor", "gdouble", `threshold`, val);
-        })
+        });
 
-        this.on("comp_makeup", val => {
+        this.on("comp_makeup", (val) => {
             this.set_gst("compressor", "gdouble", `makeup`, val);
-        })
+        });
 
-        this.on("comp_mix", val => {
+        this.on("comp_mix", (val) => {
             this.set_gst("compressor", "gdouble", `mix`, val);
-        })
+        });
 
-        this.on("comp_attack", val => {
+        this.on("comp_attack", (val) => {
             this.set_gst("compressor", "gdouble", `attack`, val);
-        })
+        });
 
-        this.on("comp_release", val => {
+        this.on("comp_release", (val) => {
             this.set_gst("compressor", "gdouble", `release`, val);
-        })
+        });
 
         // ------------ Audio Compressor ------------ //
 
@@ -176,6 +153,60 @@ class SoundProcessor extends Classes(_paNullSinkSourceBase, GstBase) {
         // ------------ Delay ------------ //
     }
 
+    startPipeline() {
+        if (this.ready && this.run) {
+            // Source
+            let _pipeline =
+                `pulsesrc device=${this._source} latency-time=${
+                    this._parent.paLatency * 1000
+                } buffer-time=${this._parent.paLatency * 1000} ! ` + // do-timestamp=false used for live audio (see: https://github.com/ros-drivers/audio_common/issues/120)
+                `audio/x-raw,rate=${this.sampleRate},format=S${
+                    this.bitDepth
+                }LE,channels=${this.channels},channel-mask=(bitmask)0x${(
+                    Math.pow(2, this.channels) - 1
+                ).toString(16)} ! audioconvert ! audiorate ! `;
+            // EQ
+            _pipeline += `equalizer-10bands name="eq" ${this._bands()} ! `;
+            // Gate
+            _pipeline += `calf-sourceforge-net-plugins-Gate name="gate" bypass=${!this
+                .gate} knee=${this.gate_knee} ratio=${
+                this.gate_ratio
+            } threshold=${this.gate_threshold} attack=${
+                this.gate_attack
+            } release=${this.gate_release} makeup=${this.gate_makeup} ! `;
+            // Compressor
+            _pipeline += `calf-sourceforge-net-plugins-Compressor name="compressor" bypass=${!this
+                .compressor} knee=${this.comp_knee} ratio=${
+                this.comp_ratio
+            } threshold=${this.comp_threshold} makeup=${this.comp_makeup} mix=${
+                this.comp_mix
+            } attack=${this.comp_attack} release=${this.comp_release} ! `;
+            // Delay
+            if (this.delay)
+                _pipeline += `queue name="delay" leaky=2 min-threshold-time=${
+                    this.delayVal * 1000000
+                } max-size-buffers=0 max-size-bytes=0 max-size-time=${
+                    (this.delayVal + 100) * 1000000
+                } ! `;
+
+            // Sink
+            _pipeline += `pulsesink device=${
+                this._sink
+            } sync=false buffer-time=${
+                this._parent.paLatency * 1000
+            } max-lateness=${this._parent.paLatency * 1000000}`;
+
+            // ------------ start sound processor ------------ //
+            this._parent.PaCmdQueue(() => {
+                this._start_gst(
+                    `node ${path.dirname(
+                        process.argv[1]
+                    )}/child_processes/SrtGstGeneric_child.js "${_pipeline}"`
+                );
+            });
+        }
+    }
+
     /**
      * Create a list of bands with their set gain
      * @param {Number} bandCount - Number of bands (Dafualt: 10)
@@ -184,12 +215,10 @@ class SoundProcessor extends Classes(_paNullSinkSourceBase, GstBase) {
     _bands(bandCount = 10) {
         let bands = "";
         for (let i = 0; i < bandCount; i++) {
-            if (!this.eq) 
-                bands += `band${i}=0 `;
-            else 
-                bands += `band${i}=${this[`band${i}`]} `;
+            if (!this.eq) bands += `band${i}=0 `;
+            else bands += `band${i}=${this[`band${i}`]} `;
         }
-            
+
         return bands;
     }
 
@@ -199,13 +228,10 @@ class SoundProcessor extends Classes(_paNullSinkSourceBase, GstBase) {
      */
     _setBands(bandCount = 10) {
         for (let i = 0; i < bandCount; i++) {
-            if (!this.eq) 
-                this.set_gst("eq", "gdouble", `band${i}`, 0);
-            else 
-                this.set_gst("eq", "gdouble", `band${i}`, this[`band${i}`]);
+            if (!this.eq) this.set_gst("eq", "gdouble", `band${i}`, 0);
+            else this.set_gst("eq", "gdouble", `band${i}`, this[`band${i}`]);
         }
     }
-
 }
 
 module.exports = SoundProcessor;
