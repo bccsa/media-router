bbdc2208563a59b98765f13918bec91f6c04e9e1
oswald <oswald@slabbert.org>
2025-02-20 16:12:20 +0200

    hls player with hlsDemuxer

--- server/controls/SrtVideoPlayer.js
+++ server/controls/SrtVideoPlayer.js
@@ -1,7 +1,7 @@
-const _paNullSinkBase = require('./_paNullSinkBase');
-const SrtBase = require('./SrtBase');
-const path = require('path');
-const { Classes } = require('../modular-dm');
+const _paNullSinkBase = require("./_paNullSinkBase");
+const SrtBase = require("./SrtBase");
+const path = require("path");
+const { Classes } = require("../modular-dm");
 
 class SrtVideoPlayer extends Classes(_paNullSinkBase, SrtBase) {
     constructor() {
@@ -13,10 +13,12 @@ class SrtVideoPlayer extends Classes(_paNullSinkBase, SrtBase) {
         super.Init();
 
         // Start external processes when the underlying pipe-source is ready (from extended class)
-        this.on('ready', ready => { this.startPipeline(); });
+        this.on("ready", (ready) => {
+            this.startPipeline();
+        });
 
         // Stop external processes when the control is stopped (through setting this.run to false)
-        this.on('run', run => {
+        this.on("run", (run) => {
             if (!run) this._stop_srt();
             this.startPipeline();
         });
@@ -24,19 +26,30 @@ class SrtVideoPlayer extends Classes(_paNullSinkBase, SrtBase) {
 
     startPipeline() {
         if (this.ready && this.run) {
-            this._parent._log('INFO', `${this._controlName} (${this.displayName}): Starting srt video decoder (gstreamer)`);
+            this._parent._log(
+                "INFO",
+                `${this._controlName} (${this.displayName}): Starting srt video decoder (gstreamer)`
+            );
 
-            let _pipeline = `srtserversrc name=${this._srtElementName} uri="${this.uri()}" wait-for-connection=false ! ` +
-            `tsparse ! tsdemux latency=1 name=t t. ! ` +
-            `h264parse ! v4l2h264dec ! ` + 
-            `queue flush-on-eos=true leaky=2 max-size-time=50000000 ! ` +
-            `kmssink sync=false async=false t. ! ` + 
-            `aacparse ! avdec_aac ! audioconvert ! ` + 
-            `queue flush-on-eos=true leaky=2 max-size-time=50000000 ! ` +
-            `pulsesink device=${this.sink} sync=false slave-method=0 processing-deadline=40000000 buffer-time=50000 max-lateness=50000000`;
+            let _pipeline =
+                `srtserversrc name=${
+                    this._srtElementName
+                } uri="${this.uri()}" wait-for-connection=false ! ` +
+                `tsparse ! tsdemux latency=1 name=t t. ! ` +
+                `h264parse ! v4l2h264dec ! ` +
+                `queue flush-on-eos=true leaky=2 max-size-time=50000000 ! ` +
+                `kmssink sync=false async=false t. ! ` +
+                `aacparse ! avdec_aac ! audioconvert ! ` +
+                `queue flush-on-eos=true leaky=2 max-size-time=50000000 ! ` +
+                `pulsesink device=${this.sink} sync=false slave-method=0 processing-deadline=40000000 buffer-time=50000 max-lateness=50000000`;
 
-            this._parent.PaCmdQueue(() => { 
-                this._start_srt(`node ${path.dirname(process.argv[1])}/child_processes/SrtGstGeneric_child.js '${_pipeline}'`, this._srtElementName);
+            this._parent.PaCmdQueue(() => {
+                this._start_srt(
+                    `node ${path.dirname(
+                        process.argv[1]
+                    )}/child_processes/SrtGstGeneric_child.js '${_pipeline}'`,
+                    this._srtElementName
+                );
             });
         }
     }
