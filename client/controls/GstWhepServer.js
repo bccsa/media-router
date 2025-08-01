035cb255c69ff4a6f6bd8525b1681cfc8fedc516
Ivan Slabbert <ivan@slabbert.org>
2025-08-02 00:14:28 +0200

    Refactor GstWhepServer class and add child process for WHEP server; update submodule configuration

--- client/controls/GstWhepServer.js
+++ client/controls/GstWhepServer.js
@@ -3,8 +3,8 @@ const SVE = require("controls/GstWhepServer/html");
 class GstWhepServer extends _paAudioSinkBase {
     constructor() {
         super();
-        this.port = 9090 // Default port for WHEP server
-        this.opusFec = false; // Enable Opus Forward Error Correction 
+        this.port = 9090; // Default port for WHEP server
+        this.opusFec = false; // Enable Opus Forward Error Correction
         this.opusFecPacketLoss = 5; // Opus FEC packet loss percentage (preset value)
         this.opusComplexity = 10; // Opus complexity level (0 - 10) where 0 is the lowest quality, and 10 is the highest quality.
         this.opusBitrate = 64; // Opus encoding target bitrate in kbps
@@ -14,20 +14,21 @@ class GstWhepServer extends _paAudioSinkBase {
     }
 
     get html() {
-        return super.html.replace('%additionalHtml%', `
+        return super.html.replace(
+            "%additionalHtml%",
+            `
             ${SVE.html()}
-        `);
+        `
+        );
     }
-
 
     Init() {
         super.Init();
-        this.setHeaderColor('#007F6A');
+        this.setHeaderColor("#007F6A");
 
         //----------------------Help Modal-----------------------------//
         // Load help from MD
         // this._loadHelpMD('controls/GstWhepServer.md');
         //----------------------Help Modal-----------------------------//
     }
-
 }
--- server/controls/GstWhepServer.js
+++ server/controls/GstWhepServer.js
@@ -1,13 +1,14 @@
-const _paNullSinkBase = require('./_paNullSinkBase');
-const path = require('path');
-const GstBase = require('./GstBase');
+const _paNullSinkBase = require("./_paNullSinkBase");
+const Spawn = require("./spawn");
+const path = require("path");
+
 const { Classes } = require("../modular-dm");
 
-class GstWhepServer extends Classes(_paNullSinkBase, GstBase) {
+class GstWhepServer extends Classes(_paNullSinkBase, Spawn) {
     constructor() {
         super();
-        this.port = 9090 // Default port for WHEP server
-        this.opusFec = false; // Enable Opus Forward Error Correction 
+        this.port = 9090; // Default port for WHEP server
+        this.opusFec = false; // Enable Opus Forward Error Correction
         this.opusFecPacketLoss = 5; // Opus FEC packet loss percentage (preset value)
         this.opusComplexity = 10; // Opus complexity level (0 - 10) where 0 is the lowest quality, and 10 is the highest quality.
         this.opusBitrate = 64; // Opus encoding target bitrate in kbps
@@ -20,21 +21,43 @@ class GstWhepServer extends Classes(_paNullSinkBase, GstBase) {
         super.Init();
 
         // Start external processes when the underlying null-sink is ready (from extended class)
-        this.on('ready', ready => { this.startPipeline(); });
+        this.on("ready", (ready) => {
+            if (ready) this.startPipeline();
+            else this.stopPipeline();
+        });
 
         // Stop external processes when the control is stopped (through setting this.run to false)
-        this.on('run', run => {
-            this.startPipeline();
+        this.on("run", (run) => {
+            if (run) this.startPipeline();
+            else this.stopPipeline();
         });
     }
-    
+
     startPipeline() {
         if (this.ready && this.run) {
+            this._start_cmd(
+                `node ${path.dirname(
+                    process.argv[1]
+                )}/child_processes/GstWhepServer_child.js '${this.source}' ${
+                    this.port
+                }`,
+                this._srtElementName
+            );
             this._parent._log(
-                    "INFO",
-                    `${this._controlName} (${this.displayName}): ready:${this.ready} run: ${this.run} port: ${this.port} opusFec: ${this.opusFec} opusFecPacketLoss: ${this.opusFecPacketLoss} opusComplexity: ${this.opusComplexity} opusBitrate: ${this.opusBitrate} opusFrameSize: ${this.opusFrameSize} rtpRed: ${this.rtpRed} rtpRedDistance: ${this.rtpRedDistance}`
-                );
-       }
+                "INFO",
+                `${this._controlName} (${this.displayName}): Started WHEP server on port ${this.port}.`
+            );
+        }
+    }
+
+    stopPipeline() {
+        if (this._whep) {
+            this._stop_cmd();
+            this._parent._log(
+                "INFO",
+                `${this._controlName} (${this.displayName}): Stopped WHEP server.`
+            );
+        }
     }
 }
 
