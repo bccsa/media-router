3a6202f60f443496c438f218b9c5ad6ad11ab6e2
oswald <oswald@slabbert.org>
2025-08-07 09:44:01 +0200

    update install scripts

--- dev/install-dependencies-bookworm-arm64.sh
+++ dev/install-dependencies-bookworm-arm64.sh
@@ -8,6 +8,10 @@ sudo apt-get -y install srt-tools
 sudo apt-get -y install net-tools
 sudo apt-get -y install git
 sudo apt-get -y install libgtk-3-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-pulseaudio calf-plugins
+sudo apt install build-essential libgirepository1.0-dev \
+    gir1.2-gtk-3.0 pkg-config
+sudo apt-get install libnice10 gstreamer1.0-nice gir1.2-gst-plugins-bad-1.0 \
+    gir1.2-gst-plugins-base-1.0
 
 git submodule update --init
 
@@ -36,4 +40,8 @@ cd ../../..
 cd server/gst_modules/GstGeneric
 node-gyp configure
 npm i
+cd ../../..
+
+cd server/gst_modules/GstWhepServer
+npm ci && npm run build
 cd ../../..
